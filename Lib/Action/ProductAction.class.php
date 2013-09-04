12bc6d8c416bcafda647f8ff4d20efc667467209
MinoWu <minowu@foxmail.com>
2013-09-04 11:41:40 +0800

    更新show方法

--- Lib/Action/ProductAction.class.php
+++ Lib/Action/ProductAction.class.php
@@ -4,16 +4,17 @@ class ProductAction extends Action
 {
 	public function index()
 	{
-		// 模拟数据库数据
 		$product = M('Product');
 		$datas = $product->select();
 
-		// echo json_encode($products);
 		$this->ajaxReturn($datas, 'JSON');
 	}
 
 	public function show()
 	{
-		echo "show";
+		$product = M('Product');
+		$datas = $product->find(1);
+
+		$this->ajaxReturn($datas, 'JSON');
 	}
 }
