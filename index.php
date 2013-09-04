dd0bf0f433545e1d648646b368f932614b70733d
MinoWu <minowu@foxmail.com>
2013-09-04 10:57:44 +0800

    添加调试模式代码

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
+// 显示错误
+// ini_set('display_errors', 1);
+// ini_set('error_reporting', E_ERROR | E_PARSE);
+
+// 调试模式
+define('APP_DEBUG', true);
+
+// 加载系统框架
 require './ThinkPHP/ThinkPHP.php';
