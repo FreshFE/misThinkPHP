0af59d86f3958eb8f6f5d87d5e81d7ead1768630
MinoWu <minowu@foxmail.com>
2013-09-05 11:33:13 +0800

    添加create, edit和delete等方法

--- Lib/Action/ProductAction.class.php
+++ Lib/Action/ProductAction.class.php
@@ -17,4 +17,61 @@ class ProductAction extends Action
 
 		$this->ajaxReturn($datas, 'JSON');
 	}
+
+	public function create()
+	{
+		// 判断是否post
+		if ($this->isPost()) {
+
+			$product = M('Product');
+			$data = $product->create();
+			$id = $product->add($data);
+
+			if ($id) {
+				$this->ajaxReturn(array('success'), 'JSON');
+			} else {
+				$this->ajaxReturn(array('failed'), 'JSON');
+			}
+		}
+	}
+
+	public function edit()
+	{
+		// 自己写一个
+		if ($this->isPost()) {
+
+			$product = M('Product');
+			$data = $product->create();
+			$id = $product->save($data);
+
+			if ($id) {
+				$this->ajaxReturn(array('success'), 'JSON');
+			} else {
+				$this->ajaxReturn(array('failed'), 'JSON');
+			}
+		}
+	}
+
+	public function remove($id)
+	{
+		$product = M('Product');
+		$result = $product->where(array('id' => $id))->delete();
+
+		if ($result) {
+			$this->ajaxReturn(array('success'), 'JSON');
+		} else {
+			$this->ajaxReturn(array('failed'), 'JSON');
+		}
+	}
 }
+ 
+
+
+
+
+
+
+
+
+
+
